d30cd3893af7fae6bbcf4f1bfbceb060512860ef
nicetiger <155900397+1NiceTiger1@users.noreply.github.com>
2024-01-10 20:21:58 +0300

    update

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function submit() {
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("formContainer").innerHTML = "<div class='hw-site__submit-message'>Спасибо! Мы с вами свяжемся в ближайшее время</div>";
+}
